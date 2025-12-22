c8d9a8e2ce393225b0774609deacd9e00d8a14e4
amdymstfa <amdymoustapha011@gmail.com>
2025-12-22 16:20:39 +0100

    Initialization of project

---
+++ src/main/java/com/logistics/intellistock/IntellistockApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.intellistock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellistockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellistockApplication.class, args);
+	}
+
+}
