25080e4c16e82d9a9436cb9723863895dcc02727
amdymstfa <amdymoustapha011@gmail.com>
2025-12-24 11:26:09 +0100

    Add StockResponse DTO

---
+++ src/main/java/com/logistics/intellistock/dto/response/StockResponse.java
@@ -0,0 +1,25 @@
+package com.logistics.intellistock.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StockResponse {
+    private Long id;
+    private Long productId;
+    private String productName;
+    private String productSku;
+    private Long warehouseId;
+    private String warehouseName;
+    private Integer quantityAvailable;
+    private Integer alertThreshold;
+    private Boolean isBelowThreshold;
+    private LocalDateTime lastRestockedAt;
+}
