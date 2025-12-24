93b6e2da23b6a00ca176f589ab6a4331b1f83256
amdymstfa <amdymoustapha011@gmail.com>
2025-12-24 11:36:07 +0100

    Add ProductService interface

---
+++ src/main/java/com/logistics/intellistock/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.logistics.intellistock.service;
+
+import com.logistics.intellistock.dto.request.CreateProductRequest;
+import com.logistics.intellistock.dto.request.UpdateProductRequest;
+import com.logistics.intellistock.dto.response.ProductAdminResponse;
+import com.logistics.intellistock.dto.response.ProductResponse;
+import com.logistics.intellistock.enums.Category;
+
+import java.util.List;
+
+public interface ProductService {
+    ProductResponse createProduct(CreateProductRequest request);
+    ProductResponse getProductById(Long id);
+    ProductAdminResponse getProductByIdAdmin(Long id);
+    List<ProductResponse> getAllProducts();
+    List<ProductAdminResponse> getAllProductsAdmin();
+    List<ProductResponse> getProductsByCategory(Category category);
+    ProductResponse updateProduct(Long id, UpdateProductRequest request);
+    void deleteProduct(Long id);
+}
