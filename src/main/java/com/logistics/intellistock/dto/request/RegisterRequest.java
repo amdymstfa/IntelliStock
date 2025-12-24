51375a5b0ed6515ca812d4ee9991712f5cf69232
amdymstfa <amdymoustapha011@gmail.com>
2025-12-24 11:24:19 +0100

    Add RegisterRequest DTO

---
+++ src/main/java/com/logistics/intellistock/dto/request/RegisterRequest.java
@@ -0,0 +1,46 @@
+package com.logistics.intellistock.dto.request;
+
+import com.logistics.intellistock.enums.Role;
+import com.logistics.intellistock.validation.ValidEmail;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Login is required")
+    @Size(min = 3, max = 50, message = "Login must be between 3 and 50 characters")
+    private String login;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$",
+        message = "Password must contain at least one uppercase, one lowercase, one number and one special character"
+    )
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50, message = "First name must not exceed 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50, message = "Last name must not exceed 50 characters")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @ValidEmail
+    private String email;
+
+    @NotNull(message = "Role is required")
+    private Role role;
+
+    private Long warehouseId;
+}
