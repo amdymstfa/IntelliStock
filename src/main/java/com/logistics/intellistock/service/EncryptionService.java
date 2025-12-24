2df4893b5fc378059c362347da8ba00d355072d9
amdymstfa <amdymoustapha011@gmail.com>
2025-12-24 11:36:07 +0100

    Add EncryptionService interface

---
+++ src/main/java/com/logistics/intellistock/service/EncryptionService.java
@@ -0,0 +1,6 @@
+package com.logistics.intellistock.service;
+
+public interface EncryptionService {
+    String encrypt(String data) throws Exception;
+    String decrypt(String encryptedData) throws Exception;
+}
