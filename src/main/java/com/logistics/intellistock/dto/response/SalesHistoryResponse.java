d1e14bf5bf2a383d9f9d9addc5aff33f14c24288
amdymstfa <amdymoustapha011@gmail.com>
2025-12-24 11:26:09 +0100

    Add SalesHistoryResponse DTO

---
+++ src/main/java/com/logistics/intellistock/dto/response/SalesHistoryResponse.java
@@ -0,0 +1,26 @@
+package com.logistics.intellistock.dto.response;
+
+import com.logistics.intellistock.enums.DayOfWeek;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SalesHistoryResponse {
+    private Long id;
+    private Long productId;
+    private String productName;
+    private Long warehouseId;
+    private String warehouseName;
+    private LocalDate saleDate;
+    private Integer quantitySold;
+    private DayOfWeek dayOfWeek;
+    private Integer month;
+    private Integer year;
+}
