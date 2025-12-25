de6deacb0b99aa80c6b74567ec66153bc056173e
jad-png <j.belassiria3993@uca.ac.ma>
2025-12-25 11:18:42 +0100

    enhance(entity): changed id type to string, and concate between first and last name to username and remove login attribute

--- src/main/java/com/logistics/intellistock/repository/UserRepository.java
+++ src/main/java/com/logistics/intellistock/repository/UserRepository.java
@@ -14,6 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByUsername(String username);
 
+  Optional<User> findByUsername(String login);
+
   Optional<User> findByEmail(String email);
 
   boolean existsByUsername(String username);
