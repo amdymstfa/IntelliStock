935dbca32c0f62e9f7dcc0d481c1e6056381ac20
jad-png <j.belassiria3993@uca.ac.ma>
2025-12-25 11:58:16 +0100

    fix(security): typo in getter method signature

--- src/main/java/com/logistics/intellistock/security/UserPrincipal.java
+++ src/main/java/com/logistics/intellistock/security/UserPrincipal.java
@@ -31,7 +31,7 @@ public class UserPrincipal implements UserDetails {
 
         return UserPrincipal.builder()
                 .id(user.getId())
-                .username(user.getUserName())
+                .username(user.getUsername())
                 .password(user.getPassword())
                 .active(user.getIsActive())
                 .authorities(authorities)
