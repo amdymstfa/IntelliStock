21f00fac813a053dab108bdd27d5f669a6629ed8
jad-png <j.belassiria3993@uca.ac.ma>
2025-12-25 11:16:22 +0100

    untracked changes

--- src/main/java/com/logistics/intellistock/repository/UserRepository.java
+++ src/main/java/com/logistics/intellistock/repository/UserRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  Optional<User> findByLogin(String login);
+  Optional<User> findByUsername(String login);
 
   Optional<User> findByEmail(String email);
 
