ba7b2d4e7cf6b46fd9d4e641aefe2b8883211a16
jad-png <j.belassiria3993@uca.ac.ma>
2025-12-25 11:18:42 +0100

    enhance(entity): changed id type to string, and concate between first and last name to username and remove login attribute

---
+++ src/main/java/com/logistics/intellistock/mapper/AuthMapper.java
@@ -0,0 +1,29 @@
+package com.logistics.intellistock.mapper;
+
+import com.logistics.intellistock.dto.request.RegisterRequest;
+import com.logistics.intellistock.dto.response.AuthResponse;
+import com.logistics.intellistock.entity.User;
+import com.logistics.intellistock.enums.Role;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", imports = {Role.class})
+public interface AuthMapper {
+
+    @Mapping(target = "token", source = "token")
+    @Mapping(target = "type", constant = "Bearer")
+    @Mapping(target = "username", source = "user.username")
+    @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "role", source = "user.role")
+    @Mapping(target = "warehouseId", source = "user.warehouse.id")
+    @Mapping(target = "warehouseName", source = "user.warehouse.name")
+    AuthResponse toAuthResponse(User user, String token);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "isActive", constant = "true")
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "warehouse", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    User toEntity(RegisterRequest request);
+}
