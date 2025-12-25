4f49d93711707a0c1a355a1ea676ad121003ca12
jad-png <j.belassiria3993@uca.ac.ma>
2025-12-25 17:09:36 +0100

    enhance(): enahnce Security Utils class, to fetch user using userPrincipal class, and not demanding it from db each time trying to check if user is admin or manager

--- src/main/java/com/logistics/intellistock/entity/User.java
+++ src/main/java/com/logistics/intellistock/entity/User.java
@@ -24,12 +24,6 @@ public class User {
   @Column(nullable = false, unique = true, length = 50)
   private String username;
 
-  @Column(nullable = false, unique = true, length = 50)
-  private String username;
-
-  @Column(nullable = false, unique = true, length = 50)
-  private String username;
-
   @Column(nullable = false, length = 255)
   private String password;
 
